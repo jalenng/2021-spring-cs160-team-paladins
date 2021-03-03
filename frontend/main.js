5fa16637b02a8d755dba260e9a7f6624a2e6cc95
zuby <zubyjaved@gmail.com>
2021-03-02 21:15:02 -0800

    fix merge conflict

--- frontend/main.js
+++ frontend/main.js
@@ -1,84 +1,45 @@
 // Modules to control application life and create native browser window
-const { ipcMain, app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
-const axios = require('axios')
 
-function createWindow() {
-
-    // Main iCare Window
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 500,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true
-        }
-
-    })
-    mainWindow.loadFile('index.html')
-    mainWindow.menuBarVisible = false
-    mainWindow.webContents.openDevTools()
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
     
-    // Handle event for when account button is clicked
-    ipcMain.handle('account-button-action', event => {
+  })
 
-        // Sign in Window
-        const signInWindow = new BrowserWindow({
-            width: 380,
-            height: 380,
-            modal: true,
-            resizable: false,
-            minimizable: false,
-            maximizable: false,
-            title: "Sign in",
-            parent: mainWindow,
-            webPreferences: {
-                preload: path.join(__dirname, 'preload.js'),
-                nodeIntegration: true
-            }
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
 
-        })
-        signInWindow.menuBarVisible = false
-        signInWindow.loadFile('sign-in/index.html')   
-        
-    })
-    
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow()
-
-    app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+  createWindow()
+  
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit()
 })
 
-// Event handler for signing in
-ipcMain.handle('sign-in', (event, username, password) => {
-
-    axios
-    .post('http://localhost:3000', {
-        username: username,
-        password: password
-    })
-    .then(res => {
-        console.log(`statusCode: ${res.statusCode}`)
-        console.log(res)
-    })
-    .catch(error => {
-        console.error(error)
-    })
-
-})
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
--- frontend/renderer.js
+++ frontend/renderer.js
@@ -1,10 +1,14 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 23b9f4f (modify scripts to run in main process through IPC events)
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+<<<<<<< HEAD
 
 const server = require('../backend/server.js')
 =======
@@ -14,3 +18,5 @@ document.getElementById('accountButton').addEventListener('click', () => {
     ipcRenderer.invoke('account-button-action')
 })
 >>>>>>> 23b9f4f0af19c299820bb563cee552c567c30fb8
+=======
+>>>>>>> parent of 23b9f4f (modify scripts to run in main process through IPC events)

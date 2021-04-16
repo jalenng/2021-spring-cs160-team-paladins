d95d9d5ad706aca1ae597dbaf6f75b667aac687c
unknown <zubyjaved@gmail.com>
2021-04-16 14:27:27 -0700

    remove title and left padding

--- frontend/src/HomeScreen.js
+++ frontend/src/HomeScreen.js
@@ -10,25 +10,10 @@ const homeStyle = {
   msUserSelect: "none",
 
   paddingTop: '30px',
-  paddingLeft: '50px',
   textAlign: 'center',
   // Align home screen in center of Electron window.
   position: 'absolute', left: '50%', top: '50%',
   transform: 'translate(-50%, -50%)'
-};
-
-const titleStyle = {
-  color: 'white',
-  marginBottom: '20px',
-}
-
-let Title = () => {
-  return (
-    <div style={titleStyle}>
-      <Text variant={'xxLarge'} block>iCare</Text>
-      <Text variant={'medium'} block>A Break Timer App for Productivity</Text>
-    </div>
-  )
 };
 
 export default class HomeScreen extends React.Component {
@@ -36,7 +21,6 @@ export default class HomeScreen extends React.Component {
   render() {
     return (    
       <div style={homeStyle}>
-        <Title />
         <Timer />
       </div>
     );

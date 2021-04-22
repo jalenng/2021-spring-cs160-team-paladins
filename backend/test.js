cccb4ea28813e88f11deed112caf86a7b51ea507
Julie Loi <36089473+JLoiLeaf@users.noreply.github.com>
2021-04-21 18:24:08 -0700

    added get /user

--- backend/test.js
+++ backend/test.js
@@ -3,7 +3,7 @@ let assert = require('assert')
 
 // Database Connection
 let db = require('./db.js')
-const userDB = new db("localhost", "newuser", "password", "iCare");
+const userDB = new db("localhost", "newuser", "", "iCare");
 
 // API Methods
 let apiM = require('./api_methods.js');

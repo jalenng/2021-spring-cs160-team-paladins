7339fd03f3667e8f39fd7484478cb95692e6cc83
shiyunlian <shiyunlian@gmail.com>
2021-04-26 19:28:00 -0700

    timer test

--- frontend/src/Timer.js
+++ frontend/src/Timer.js
@@ -10,7 +10,7 @@ import { getTheme } from "@fluentui/react";
 
 import Circle from "react-circle";
 
-const buttonStyle = { borderRadius: '20px', width: '40px', height: '40px' };
+const buttonStyle = { borderRadius: "20px", width: "40px", height: "40px" };
 const buttonIconClass = mergeStyles({ fontSize: 24, height: 24, width: 24 });
 
 export default class Timer extends React.Component {
@@ -21,20 +21,25 @@ export default class Timer extends React.Component {
       totalDuration: "",
       remainingTime: "",
       endTimeString: "",
-      state: ""
+      state: "",
     };
   }
 
   componentDidMount() {
     timer.eventSystem.on("update", (event, timerStatus) => {
-      let remainingMinutes = Math.floor(timerStatus.remainingTime / 60000).toString();
-      let remainingSeconds = Math.floor((timerStatus.remainingTime % 60000) / 1000);
+      let remainingMinutes = Math.floor(
+        timerStatus.remainingTime / 60000
+      ).toString();
+      let remainingSeconds = Math.floor(
+        (timerStatus.remainingTime % 60000) / 1000
+      );
       remainingSeconds = ("00" + remainingSeconds).substr(-2, 2);
 
-      let endHours = timerStatus.endDate.getHours()
-      endHours = endHours === 0
-        ? "12"
-        : endHours > 12
+      let endHours = timerStatus.endDate.getHours();
+      endHours =
+        endHours === 0
+          ? "12"
+          : endHours > 12
           ? (endHours % 12).toString()
           : endHours.toString();
       let endMinutes = timerStatus.endDate.getMinutes();
@@ -53,33 +58,42 @@ export default class Timer extends React.Component {
     setInterval(timer.getStatus, 100);
   }
 
-  resetBtnClick() { timer.reset() };
+  resetBtnClick() {
+    timer.reset();
+  }
 
-  popOutBtnClick() { showPopup.timer() };
+  popOutBtnClick() {
+    showPopup.timer();
+  }
 
-  handleEndBtn() { timer.end() };
+  handleEndBtn() {
+    timer.end();
+  }
 
-  togglePause() { timer.toggle() };
+  togglePause() {
+    timer.toggle();
+  }
 
   render() {
-
     return (
       <div>
         <Stack vertical tokens={{ childrenGap: 8 }} horizontalAlign="center">
-
           {/* Timer display */}
-          <div style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}>
-
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             {/* Circular progress bar */}
             <Circle
               animate={false}
               size={300}
               lineWidth={20}
-              progress={(this.state.remainingTime / this.state.totalDuration) * 100}
+              progress={
+                (this.state.remainingTime / this.state.totalDuration) * 100
+              }
               progressColor={getTheme().palette.themePrimary}
               bgColor={getTheme().palette.neutralLighter}
               roundedStroke={true}
@@ -89,44 +103,45 @@ export default class Timer extends React.Component {
             {/* Timer information */}
             <div className="time" style={{ position: "absolute" }}>
               <Stack vertical horizontalAlign="center">
-
                 {/* Remaining time */}
                 <Text variant={"xxLarge"} style={{ fontSize: "4rem" }} block>
                   <div id="remainingTimeText">
-                    
-                  {this.state.remainingTimeString}
+                    {this.state.remainingTimeString}
                   </div>
                 </Text>
 
                 {/* End time - show only if the timer is running*/}
-                {this.state.state === "running" &&
+                {this.state.state === "running" && (
                   <TooltipHost content="End time">
                     <DefaultButton
                       style={{ ...buttonStyle, height: "28px" }}
-                      iconProps={{ iconName: 'Ringer' }}
+                      iconProps={{ iconName: "Ringer" }}
                       text={this.state.endTimeString}
                       disabled
                     />
                   </TooltipHost>
-                }
+                )}
               </Stack>
             </div>
-
           </div>
 
           {/* Timer controls */}
           <Stack horizontal tokens={{ childrenGap: 20 }}>
-
             {/* Toggle button */}
-            <TooltipHost content={this.state.state === "running" ? "Pause" : "Start"}>
+            <TooltipHost
+              content={this.state.state === "running" ? "Pause" : "Start"}
+            >
               <PrimaryButton
                 id="toggleButton"
                 disabled={this.state.state === "idle"}
                 onClick={this.togglePause}
                 style={buttonStyle}
                 onRenderText={() => {
-                  let iconName = this.state.state === "running" ? "Pause" : "Play";
-                  return (<FontIcon iconName={iconName} className={buttonIconClass} />)
+                  let iconName =
+                    this.state.state === "running" ? "Pause" : "Play";
+                  return (
+                    <FontIcon iconName={iconName} className={buttonIconClass} />
+                  );
                 }}
               />
             </TooltipHost>
@@ -134,40 +149,42 @@ export default class Timer extends React.Component {
             {/* Other actions */}
             <TooltipHost content={"More"}>
               <DefaultButton
-                disabled={this.state.state === "blocked" || this.state.state === "idle"}
+                id="buttonGroup"
+                disabled={
+                  this.state.state === "blocked" || this.state.state === "idle"
+                }
                 style={buttonStyle}
                 onRenderText={() => {
-                  return (<FontIcon iconName="More" className={buttonIconClass} />)
+                  return (
+                    <FontIcon iconName="More" className={buttonIconClass} />
+                  );
                 }}
                 menuProps={{
                   items: [
                     {
                       key: "resetTimer",
-                      text: 'Reset timer',
-                      iconProps: { iconName: 'Refresh' },
-                      onClick: this.resetBtnClick
+                      text: "Reset timer",
+                      iconProps: { iconName: "Refresh" },
+                      onClick: this.resetBtnClick,
                     },
                     {
                       key: "popOutTimer",
-                      text: 'Pop out',
-                      iconProps: { iconName: 'MiniExpand' },
-                      onClick: this.popOutBtnClick
+                      text: "Pop out",
+                      iconProps: { iconName: "MiniExpand" },
+                      onClick: this.popOutBtnClick,
                     },
                     {
                       key: "startBreak",
-                      text: 'Start break (for testing purposes)',
-                      iconProps: { iconName: 'FastForward' },
-                      onClick: this.handleEndBtn
-                    }
-                  ]
+                      text: "Start break (for testing purposes)",
+                      iconProps: { iconName: "FastForward" },
+                      onClick: this.handleEndBtn,
+                    },
+                  ],
                 }}
               />
             </TooltipHost>
-
           </Stack>
-
         </Stack>
-
       </div>
     );
   }

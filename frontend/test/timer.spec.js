938586828f9d09d33a767cbe20e4a44571e51436
jalenng <jalen_ng@outlook.com>
2021-04-26 20:54:50 -0700

    Uncomment and fix conflict in test scripts

--- frontend/test/timer.spec.js
+++ frontend/test/timer.spec.js
@@ -1,9 +1,8 @@
-// const Application = require("spectron").Application;
-// const assert = require("assert");
-// const electronPath = require("electron");
-// const path = require("path");
+const Application = require("spectron").Application;
+const assert = require("assert");
+const electronPath = require("electron");
+const path = require("path");
 
-<<<<<<< HEAD
 /**
  * describe()
  *  - Describes a group/suite of test cases
@@ -106,81 +105,3 @@ function wait(milliseconds) {
     }, milliseconds);
   });
 }
-=======
-// /**
-//  * describe()
-//  *  - Describes a group/suite of test cases
-//  *  - Test cases in a test suite share the same precondition setup
-//  * 
-//  * beforeEach()
-//  *  - A hook to define the precondition
-//  *  - Runs once before running each test case
-//  * 
-//  * it()
-//  *  - Describes a test case in the test suite
-//  *  - Each test suite can have multiple test cases (or multiple it()'s)
-//  * 
-//  * afterEach()
-//  *  - A hook to perform cleanups
-//  *  - Runs once after each test case
-//  * 
-//  */
-
-// describe("Timer functionality", function () {
-//     this.timeout(60000); // Give 60 seconds for entire test suite to run
-//     this.app;
-
-//     beforeEach(function () {
-//         this.timeout(30000); // Give 30 seconds for Electron to open
-//         this.app = new Application({
-//             path: electronPath,
-//             args: [path.join(__dirname, "..")],
-//             requireName: 'electronRequire'
-//         });
-//         return this.app.start();
-//     });
-
-//     it("Start and pause timer with button", async function () {
-//         const isVisible = await this.app.browserWindow.isVisible();
-//         assert.strictEqual(isVisible, true);
-
-//         const timerComp = await this.app.client.react$('Timer')
-//         const toggleButton = await timerComp.react$('button', { // FluentUI's 'Buttons' are 'buttons'
-//             props: { id: 'toggleButton' }
-//         });
-//         const remainingTimeComp = await timerComp.react$('div', {
-//             props: { id: 'remainingTimeText'}
-//         })
-
-//         // Timer should be running by default
-
-//         await toggleButton.click(); // Pause the timer
-
-//         // Check if the timer's remaining time is changing
-//         const remainingTimeText = await remainingTimeComp.getText()
-//         await wait(3000);   // Wait 3 seconds
-//         assert.strictEqual(remainingTimeText, await remainingTimeComp.getText()) // Remaining time shouldn't change
-
-//         await toggleButton.click(); // Unpause the timer
-
-//         // Check if the timer's remaining time is changing
-//         await wait(3000);   // Wait 3 seconds
-//         assert.notStrictEqual(remainingTimeText, await remainingTimeComp.getText()) // Remaining time should change
-
-//         await wait(3000);   // Wait 3 seconds
-
-//     });
-
-//     afterEach(function () {
-//         if (this.app && this.app.isRunning())
-//             return this.app.stop();
-//     });
-// });
-
-// /* Helper function to pause execution for a given amount of time */
-// function wait(milliseconds) {
-//     return new Promise(function (resolve, reject) {
-//         setTimeout(() => { resolve() }, milliseconds)
-//     })
-// }
->>>>>>> 531795f7361daa1ba7fdaa3735fb98efb8c3c91d

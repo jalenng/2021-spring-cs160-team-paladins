a039380188d9ba873279e6fd2da9e750a1746a9f
unknown <zubyjaved@gmail.com>
2021-05-08 00:05:25 -0700

    get usage statistics given date and list of usage objects (app or timer)

--- frontend/src/usage/DailyTimerUsage.js
+++ frontend/src/usage/DailyTimerUsage.js
@@ -9,122 +9,23 @@ export default class DailyTimerUsage extends React.Component {
   constructor(props) {
     super(props);
     let usage = new Usage();
-    this.timerUsage = usage.getDailyUsage(usage.fetched.timerUsage);
+    this.fetched = usage.getUsage(usage.todayFormatted, usage.state.fetched.timerUsage);
+    console.log(this.fetched);
 
-    this.screenTime = this.timerUsage.screenTime;
-    this.timerCount = this.timerUsage.timerCount;
+    this.screenTime = 0;
+    this.timerCount = 0;
+    if (this.fetched != null) {
+      this.screenTime += this.fetched.screenTime;
+      this.timerCount += this.fetched.timerCount;
+    }
   }
 
   render() {
     return (
       <div>
-        <div>
-          {/* Daily screen time */}
-          <Bar
-            data={{
-              data: this.screenTime,
-              labels: [
-                this.weekday,
-              ],
-              datasets: [
-                {
-                  label: "Daily Screen Usage (Seconds)",
-                  data: [this.screenTime],
-                  backgroundColor: [
-                    "rgba(72, 121, 240, 1)",
-                  ],
-                },
-              ],
-            }}
-            height={400}
-            width={30}
-            options={{
-              title: {
-                display: true,
-                text: "Screen Usage (seconds)",
-                fontColor: "#FFFFFF",
-                fontSize: 15,
-                padding: 10,
-              },
-              scales: {
-                yAxes: [
-                  {
-                    ticks: {
-                      beginAtZero: true,
-                      fontColor: "#FFFFFF",
-                    },
-                  },
-                ],
-                xAxes: [
-                  {
-                    ticks: { fontColor: "#FFFFFF" },
-                  },
-                ],
-              },
-              maintainAspectRatio: false,
-              legend: {
-                position: "right",
-                labels: {
-                  fontSize: 15,
-                  fontColor: "#FFFFFF",
-                },
-              },
-            }}
-          />
-        </div>
-        <div>
-        <Bar
-            data={{
-              labels: [
-                this.weekday,
-              ],
-              datasets: [
-                {
-                  label: "Breaks",
-                  data: [this.timerCount],
-                  backgroundColor: [
-                    "rgba(72, 121, 240, 1)",
-                  ],
-                },
-              ],
-            }}
-            height={400}
-            width={30}
-            options={{
-              title: {
-                display: true,
-                text: "# of breaks",
-                fontColor: "#FFFFFF",
-                fontSize: 15,
-                padding: 10,
-              },
-              scales: {
-                yAxes: [
-                  {
-                    ticks: {
-                      beginAtZero: true,
-                      fontColor: "#FFFFFF",
-                    },
-                  },
-                ],
-                xAxes: [
-                  {
-                    ticks: { fontColor: "#FFFFFF" },
-                  },
-                ],
-              },
-              maintainAspectRatio: false,
-              legend: {
-                position: "right",
-                labels: {
-                  fontSize: 15,
-                  fontColor: "#FFFFFF",
-                },
-              },
-            }}
-          />
-  </div>
-</div>
+        <h1>You've been using iCare for {this.screenTime} seconds today.</h1>
+        <h1>You've taken {this.timerCount} breaks today.</h1>
+      </div>
     );
   }
 }
